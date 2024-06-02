1bf8745540b88e0050550d89a40f510fdb3708ec
Hasib <hasibislam2k18@gmail.com>
2024-06-02 20:45:38 +0600

    Initial commit

---
+++ lib/screen/onboarding/email_verification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class EmailVerification extends StatefulWidget {
+  const EmailVerification({super.key});
+
+  @override
+  State<EmailVerification> createState() => _EmailVerificationState();
+}
+
+class _EmailVerificationState extends State<EmailVerification> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
